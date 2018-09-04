741654e944b2bc7b13236f7f3faa0b76db722df0
L Daniel Swakman <d.swakman@gmail.com>
2018-09-04 16:35:59 +0200

    different form types

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+	$('.form-section .field--radio label').click(function() {
+
+		$parent = $(this).closest('.form-section');
+
+		if($parent.hasClass('isActive')) {
+
+			setTimeout(function() {
+				$parent.removeClass('isActive');
+				$parent.next('.form-section').addClass('isActive');
+			}, 500);
+
+		}
+	});
+
+});
