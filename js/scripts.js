aca25d1cbbfba1f755d196d0a5388549c79383d1
L Daniel Swakman <d.swakman@gmail.com>
2018-10-01 17:56:41 +0200

    test with canvas for team page

--- js/scripts.js
+++ js/scripts.js
@@ -14,8 +14,41 @@ $(document).ready(function() {
 		}
 	});
 
-	if($('.partners-grid')) {
-		console.log('found!');
-	}
-
 });
+
+
+scrollevents = 'ready scroll resize scrollstart scrollstop';
+$(document).on(scrollevents, function() { drawPartners(); });
+
+function drawPartners() {
+	if($('.partners-grid').length > 0) {
+
+		$grid = $('.partners-grid').first();
+		$items = $grid.find('.box');
+
+	  var partners_canvas = document.getElementById('partners_canvas');
+	  var context = partners_canvas.getContext('2d');
+
+	  context.clearRect(0, 0, partners_canvas.width, partners_canvas.height);
+
+		$prevX = 0;
+		$prevY = 0;
+		console.log('container',$('#partners_canvas').offset().left, $('#partners_canvas').offset().top);
+
+		$items.each(function() {
+			$pointX = $(this).offset().left - $('#partners_canvas').offset().left + $(this).outerWidth()/2;
+			$pointY = $(this).offset().top - $('#partners_canvas').offset().top + $(this).outerHeight()/2;
+
+			$prevX = $pointX;
+			$prevY = $pointY;
+
+		  context.moveTo($('#partners_canvas').outerWidth()/2 - 20, $('#partners_canvas').outerHeight()/2 + 30);
+		  context.lineTo($pointX, $pointY);
+
+			console.log($pointX, $pointY);
+		});
+		context.strokeStyle = "#E7EDEF";
+		context.lineWidth = 5;
+		context.stroke();
+	}
+}
