a61116aecb40b33d235cb90559900f743530697c
L Daniel Swakman <d.swakman@gmail.com>
2018-10-29 14:28:29 +0100

    service table: thead & special chars

--- partials/service-comparison-table.php
+++ partials/service-comparison-table.php
@@ -1,9 +1,12 @@
-<div class="comparison">
+<div class="comparison isHidden">
 
     <?php
-    echo $table_no;
+
+    // get Tablepress table ID based on passed $table_no variable
+    $table_no = (isset($table_no)) ? $table_no : 1;
     $table_id = get_field('group_' . $table_no . '_table');
 
+    // get Tablepress table
     $table = tablepress_get_table([
         'id' => $table_id,
         'use_datatables' => false,
@@ -11,8 +14,9 @@
         'alternating_row_colors' => false,
         'first_column_th' => true,
     ]);
-    ?>
 
-    <?php echo parseTablePressTable($table) ?>
+    // parse table data (custom) and echo result
+    echo parseTablePressTable(utf8_decode($table));
+    ?>
 
 </div>
