15cba0d3098a9c2b6a69ab76c43f5de85cb0f851
Samantha Fagan <suprsam@gmail.com>
2018-10-29 15:54:46 +0100

    mostly knowledge base work

--- index.php
+++ index.php
@@ -14,38 +14,104 @@
  * @since 1.0
  * @version 1.0
  */
-/**
- *  * Template Name: Blog
- */
+
 
 get_header(); ?>
+    
+<?php
+    if ( have_posts() ) :
+
+    /* Start the Loop */
+    while ( have_posts() ) : the_post();
+        ?>
+<section id="categories" class="load-fadein">
+<?php wp_list_categories( array(
+        'orderby'    => 'name',
+        'exclude'    => array( 239, 241 )
+    ) ); ?> 
+</section>
 
 <section id="featured" class="content load-movein-btm load-delay-03s">
-	<div class="row">
-		<!-- Featured posts -->
-  </div>
+    <div class="row">
+        <div class="col-xs-12 col-md-4">
+        <?php
+        $myPosts = new WP_Query();
+        $myPosts->query('showposts=5');
+
+        while ($myPosts->have_posts()) : $myPosts->the_post();
+           the_title();
+           the_content();
+        endwhile;
+            ?>            
+            <?php query_posts("showposts=3&cat=239,241"); ?>
+        </div>
+    </div>
 </section>
 
-<section id="articles" class="bg-greylightest content load-movein-btm load-delay-05s">
-  <div class="row row--nopadding">
 
-		<?php if ( have_posts() ) : ?>
 
-			<? while ( have_posts() ) : the_post(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php
+		if ( 'post' === get_post_type() ) {
+			echo '<div class="entry-meta">';
+				if ( is_single() ) {
+					twentyseventeen_posted_on();
+				} else {
+					echo twentyseventeen_time_link();
+					twentyseventeen_edit_link();
+				};
+			echo '</div><!-- .entry-meta -->';
+		};
 
-        <a href="<?php echo get_post_permalink() ?>" class="col-xs-12 col-sm-6 post-preview">
-          <figure class="post-preview__figure"><img src="https://picsum.photos/458/352" alt="" /></figure>
-          <div class="post-preview__text">
-            <h3><?php echo get_the_title() ?></h3>
-            <?php the_excerpt() ?>
-          </div>
-        </a>
+		if ( is_single() ) {
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		} elseif ( is_front_page() && is_home() ) {
+			the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+		} else {
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		}
+		?>
+	</header><!-- .entry-header -->
 
-			<?php endwhile; ?>
+	<?php if ( '' !== get_the_post_thumbnail() && ! is_single() ) : ?>
+		<div class="post-thumbnail">
+			<a href="<?php the_permalink(); ?>">
+				<?php the_post_thumbnail( 'twentyseventeen-featured-image' ); ?>
+			</a>
+		</div><!-- .post-thumbnail -->
+	<?php endif; ?>
 
-		<? endif ?>
+	<div class="entry-content">
+		<?php
+		/* translators: %s: Name of current post */
+		the_content( sprintf(
+			__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
+			get_the_title()
+		) );
 
-	</div>
-</section>
+		wp_link_pages( array(
+			'before'      => '<div class="page-links">' . __( 'Pages:', 'twentyseventeen' ),
+			'after'       => '</div>',
+			'link_before' => '<span class="page-number">',
+			'link_after'  => '</span>',
+		) );
+		?>
+        
+	</div><!-- .entry-content -->
+
+</article><!-- #post-## -->
+            <?php
+				endwhile;
+
+			else :
+
+				get_template_part( 'template-parts/post/content', 'none' );
+
+			endif;
+			?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
 
 <?php get_footer();
