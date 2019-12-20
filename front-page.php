c41ee18fbfb386c99acf433b93b3fe90264e7eb9
ldanielswakman <d.swakman@gmail.com>
2019-12-20 13:31:04 +0100

    Updated homepage template

--- front-page.php
+++ front-page.php
@@ -16,37 +16,54 @@ get_header(); ?>
 
 <?php if (have_posts())  : while (have_posts()) : the_post(); ?>
 
-
-<section id="hero-post">
-	<div class="row row--nopadding">
+<section class="section--post-hero">
+	<div class="row row--nopadding content">
 		<div class="col-xs-10 col-sm-6 col-md-7 col-lg-5">
-			<div class="box load-movein-btm load-delay-07s">
-            	<blockquote class="blockquote--huge">
-					<?php the_field('home_headline'); ?>
-	            </blockquote>
-				<a href="<?php the_field('first_cta_link'); ?>" class="button button--circle button--large button--primary button--outline"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/arrow-down.svg" alt="" /></a>
+			<blockquote class="blockquote--huge">
+				<?php the_field('home_headline'); ?>
+			</blockquote>
+		</div>
+	</div>
+</section>
+
+<section id="audiences">
+	<div class="row content">
+
+		<?php $audience_query = new WP_Query([
+			'post_type' => 'page',
+			'post_status' => 'publish',
+			'meta_key' => '_wp_page_template',
+			'meta_value' => 'page-audiences-single.php',
+			'posts_per_page' => 4
+		])
+		?>
+
+		<?php while ( $audience_query->have_posts() ) : $audience_query->the_post(); ?>
+		
+			<div class="col-xs-6 col-lg-3">
+				<a href="<?php the_permalink() ?>" class="audience-tile">
+					<figure class="audience-tile__bubble">
+						<?php the_post_thumbnail(); ?>	
+					</figure>
+					<h3><?php the_title(); ?></h3>
+				</a>
 			</div>
-        </div>
+
+		<?php endwhile; ?>
+		<?php wp_reset_postdata(); ?>
+
 	</div>
 </section>
 
 
 <section id="intro">
-  <div class="row row--nopadding">
+	<div class="row row--nopadding">
 		<div class="col-xs-12 col-lg-8 col-lg-offset-2 content">
 			<blockquote class="blockquote--large"><p><?php the_field('second_home_headline'); ?></p></blockquote>
 			<br/>
 			<?php the_field('first_home_blurb'); ?>
 		</div>
 	</div>
-</section>
-
-
-<section id="services_preview">
-	<div class="content">
-		<?php the_field('services_section_intro'); ?>
-	</div>
-			<?php the_field('services_carousel'); ?>
 </section>
 
 <section id="knowledge_preview">
@@ -66,12 +83,12 @@ get_header(); ?>
 			</div>
 		</div>
 
-    <div class="col-xs-12 col-sm-6 content">
+		<div class="col-xs-12 col-sm-6 content">
 
-			<? $the_query = new WP_Query(['posts_per_page' => 3]); ?>
+			<?php $the_query = new WP_Query(['posts_per_page' => 3]); ?>
 
 			<?php if ( $the_query->have_posts() ) : ?>
-			  <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+				<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 				
 					<a href="<?php the_permalink() ?>" class="post-preview">
 
@@ -85,14 +102,14 @@ get_header(); ?>
 
 					</a>
 
-			  <?php endwhile; ?>
-			  <?php wp_reset_postdata(); ?>
+				<?php endwhile; ?>
+				<?php wp_reset_postdata(); ?>
 
 			<?php else : ?>
-			  <p><?php __('No Articles'); ?></p>
+				<p><?php __('No Articles'); ?></p>
 			<?php endif; ?>
 
-    </div>
+		</div>
 
 	</div>
 </section>
